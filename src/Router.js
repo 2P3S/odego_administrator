c70adc851eaefe72a77c4eb8110bca5bd26ca5b0
d556f8 <l.juhyeonni@gmail.com>
2023-03-21 00:46:53 +0900

    initialize repo

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/Main";
+import SigninPage from "./pages/Auth/Signin";
+import SignupPage from "./pages/Auth/SignUp";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
