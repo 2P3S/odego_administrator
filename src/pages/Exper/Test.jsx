f957fb4cd0b69ceb759d5db27e4d12a05fbd6aab
d556f8 <l.juhyeonni@gmail.com>
2023-03-21 01:42:38 +0900

    initialize repo and route

---
+++ src/pages/Exper/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../../components/Nav";
+
+const TestPage = () => {
+  return (
+    <div>
+      <div>test page</div>
+      <Nav />
+    </div>
+  );
+};
+
+export default TestPage;
