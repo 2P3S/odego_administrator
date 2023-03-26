c4d9ddc9ab47d34d24c7e4f1eaff4ededa58f4b2
d556f8 <l.juhyeonni@gmail.com>
2023-03-26 18:32:57 +0900

    FEAT: added display component

--- src/pages/Exper/Test.jsx
+++ src/pages/Exper/Test.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Aside from "../../components/Panel/Aside";
+import Display from "../../components/Panel/Display";
 
 const TestPage = () => {
   return (
     <div>
       <Aside />
+      <Display></Display>
     </div>
   );
 };
