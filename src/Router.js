913f8982dd8f5ebc22988da612b360dbeb54d5cd
d556f8 <l.juhyeonni@gmail.com>
2023-03-26 15:32:37 +0900

    MODIFY: set routes

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/Main";
-import SigninPage from "./pages/Auth/Signin";
-import SignupPage from "./pages/Auth/Signup";
+import HomePage from "./pages/Home";
+import MainPage from "./pages/Panel/Main";
+import NoticePage from "./pages/Panel/Notice";
+import StudentInfoPage from "./pages/Panel/StudentInfo";
+import SettingsPage from "./pages/Panel/Settings";
+import SigninPage from "./pages/Auth/SignIn";
+import SignupPage from "./pages/Auth/SignUp";
 import TestPage from "./pages/Exper/Test";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/test" element={<TestPage />} />
+
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/notice" element={<NoticePage />} />
+        <Route path="/student-info" element={<StudentInfoPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
   );
