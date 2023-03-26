eab2d2a2b63c5c89410a092b1a3a2de047c3f671
d556f8 <l.juhyeonni@gmail.com>
2023-03-26 16:54:05 +0900

    init tailwindcss

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import App from "./App";
-import "./index.css";
+import "./input.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
