313f7f9ca0cebc390b9d82a7fb579b0b04dc7e9e
d556f8 <l.juhyeonni@gmail.com>
2023-03-26 18:31:58 +0900

    FIXED: property name

--- src/components/Panel/UserCard.jsx
+++ src/components/Panel/UserCard.jsx
@@ -3,17 +3,17 @@ import { ReactComponent as LogoIcon } from "../../assets/logo/odego-logo.svg";
 
 const User = ({ professorName }) => {
   return (
-    <div class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
       <LogoIcon style={{ width: 100, height: 100 }} />
-      <span class="ml-3">{professorName}</span>
+      <span className="ml-3">{professorName}</span>
     </div>
   );
 };
 
 const Subject = ({ classId }) => {
   return (
-    <div class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-      <span class="ml-3">담당 클래스: {classId}</span>
+    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+      <span className="ml-3">담당 클래스: {classId}</span>
     </div>
   );
 };
