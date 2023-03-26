5d3c6f67155c1aadd00366b2dc99aa87629f3094
d556f8 <l.juhyeonni@gmail.com>
2023-03-26 19:42:55 +0900

    FIXED: err typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-=======
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwind-scrollbar-hide")],
->>>>>>> origin/feature
 };
