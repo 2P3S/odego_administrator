36ab00307062104f412c0ff444525118ca6d38cd
d556f8 <l.juhyeonni@gmail.com>
2023-04-03 18:45:45 +0900

    Feat: modifed components

--- src/pages/Panel/Main.jsx
+++ src/pages/Panel/Main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Aside from "../../components/Panel/Aside";
+import Display from "../../components/Panel/Display";
+import AttendanceSection from "../../components/Panel/Main/AttendanceSection";
+import NoticeSection from "../../components/Panel/Main/NoticeSection";
 
 const MainPage = () => {
   return (
     <div>
       <Aside />
+      <Display>
+        <AttendanceSection />
+        <NoticeSection />
+      </Display>
     </div>
   );
 };
