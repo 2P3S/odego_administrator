8729c703b4a13e6212a33390279e327535f011ac
d556f8 <l.juhyeonni@gmail.com>
2023-04-03 18:44:41 +0900

    Feat: add section component NoticeSection

---
+++ src/components/Panel/Main/NoticeSection.jsx
@@ -0,0 +1,12 @@
+import TitleBadge from "../../TitleBadge";
+const NoticeSection = () => {
+  return (
+    <div>
+      <div className="header-sm">
+        <TitleBadge color="blue">공지사항</TitleBadge>
+      </div>
+    </div>
+  );
+};
+
+export default NoticeSection;
