c0a60640dfdc8bb62e3ed4bc8a7af24690a098d0
d556f8 <l.juhyeonni@gmail.com>
2023-04-03 18:41:58 +0900

    Feat: add attr `seq` to TableItem

--- src/components/Panel/Table.jsx
+++ src/components/Panel/Table.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 const TableItem = ({
+  seq,
   info: { studentId, classId, studentName, studentContact },
 }) => {
   return (
@@ -21,7 +22,7 @@ const TableItem = ({
           scope="row"
           className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
         >
-          {/* TODO: seq num must be display here */}
+          {seq}
         </th>
         <td className="px-6 py-4">{studentName}</td>
         <td className="px-6 py-4">{classId}</td>
@@ -35,7 +36,14 @@ const TableItem = ({
   );
 };
 
-const Table = ({ data }) => {
+const Table = ({ type, data }) => {
+  const attr = [];
+  if (type === "student") {
+    attr.push("순번", "이름", "학년", "학번", "연락처", "출석률");
+  } else if (type === "notice") {
+    attr.push("번호", "제목", "작성자", "작성일");
+  }
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -53,97 +61,19 @@ const Table = ({ data }) => {
                 </label>
               </div>
             </th>
-            <th scope="col" className="px-6 py-3">
-              순번
-            </th>
-            <th scope="col" className="px-6 py-3">
-              이름
-            </th>
-            <th scope="col" className="px-6 py-3">
-              학년
-            </th>
-            <th scope="col" className="px-6 py-3">
-              학번
-            </th>
-            <th scope="col" className="px-6 py-3">
-              연락처
-            </th>
-            <th scope="col" className="px-6 py-3">
-              출석률
-            </th>
+            {attr.map((item, idx) => {
+              return (
+                <th key={idx} scope="col" className="px-6 py-3">
+                  {item}
+                </th>
+              );
+            })}
           </tr>
         </thead>
         {data.map((info, idx) => {
-          return <TableItem key={idx} info={info} />;
+          return <TableItem key={idx} seq={idx + 1} info={info} />;
         })}
       </table>
-      <nav
-        className="flex items-center justify-between pt-4"
-        aria-label="Table navigation"
-      >
-        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-          Showing{" "}
-          <span className="font-semibold text-gray-900 dark:text-white">
-            1-10
-          </span>{" "}
-          of{" "}
-          <span className="font-semibold text-gray-900 dark:text-white">
-            1000
-          </span>
-        </span>
-        <ul className="inline-flex items-center -space-x-px">
-          <li>
-            <a
-              href="#"
-              className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              <span className="sr-only">Previous</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              1
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              <span className="sr-only">Next</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
