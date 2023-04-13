38e8cca303ba786e554af1e99def8dfef7eb978c
d556f8 <l.juhyeonni@gmail.com>
2023-04-13 20:46:03 +0900

    Feat: define `notices` state in NoticeSection component

--- src/components/Panel/Main/NoticeSection.jsx
+++ src/components/Panel/Main/NoticeSection.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import TitleBadge from "../../TitleBadge";
 
 const NoticeItem = ({ title, date }) => {
@@ -12,6 +13,49 @@ const NoticeItem = ({ title, date }) => {
 };
 
 const NoticeSection = () => {
+  const [notices, setNotices] = useState([
+    {
+      id: 1,
+      title: "테스트1",
+      date: "2023.04.06",
+    },
+    {
+      id: 2,
+      title: "테스트2",
+      date: "2023.05.02",
+    },
+    {
+      id: 3,
+      title: "테스트3",
+      date: "2023.05.23",
+    },
+    {
+      id: 4,
+      title: "테스트4",
+      date: "2023.05.23",
+    },
+    {
+      id: 5,
+      title: "테스트5",
+      date: "2023.05.23",
+    },
+    {
+      id: 6,
+      title: "테스트6",
+      date: "2023.05.23",
+    },
+  ]);
+
+  /*  */
+  // useEffect(() => {
+  //   const getNotices = async () => {
+  //     const response = await fetch("http://localhost:8080/api/notice");
+  //     const data = await response.json();
+  //     setNotices(data);
+  //   };
+  //   getNotices();
+  // }, []);
+
   return (
     <div className="notice-section">
       <div className="header-sm">
@@ -19,9 +63,16 @@ const NoticeSection = () => {
       </div>
       <div className="w-96">
         <ul className="flex flex-col gap-2">
-          <NoticeItem title="1234" date="2023.04.06" />
-          <NoticeItem title="테스트2" date="2023.05.02" />
-          <NoticeItem title="테스트3" date="2023.05.23" />
+          {/* TODO: slice 갯수 커스텀 */}
+          {notices.slice(0, 5).map(notice => {
+            return (
+              <NoticeItem
+                key={notice.id}
+                title={notice.title}
+                date={notice.date}
+              />
+            );
+          })}
         </ul>
       </div>
     </div>
