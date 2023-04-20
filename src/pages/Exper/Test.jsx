dc02a35d7fa7f60cbce8107560a31bada2cf46cf
d556f8 <l.juhyeonni@gmail.com>
2023-04-20 11:48:13 +0900

    refactor(test-page): rearrange components

--- src/pages/Exper/Test.jsx
+++ src/pages/Exper/Test.jsx
@@ -1,3 +1,5 @@
+import "ka-table/style.css";
+
 import React from "react";
 import Aside from "@Components/Panel/Aside";
 import Display from "@Components/Panel/Display";
@@ -9,43 +11,13 @@ import PagingDemo from "@Components/Panel/Table3";
 const TestPage = () => {
   // FIXME: must be Implement with Redux
   // TODO: Impliment pagination
-  const data = [
-    {
-      studentId: "2201013",
-      classId: "2-wdj",
-      studentContact: "010-1234-5678",
-      studentName: "강주원",
-    },
-    {
-      studentId: "2201014",
-      classId: "2-wdj",
-      studentContact: "010-4567-8901",
-      studentName: "김유민",
-    },
-    {
-      studentId: "2201015",
-      classId: "2-wdj",
-      studentContact: "010-2234-7789",
-      studentName: "이주현",
-    },
-    {
-      studentId: "2201392",
-      classId: "2-wdj",
-      studentContact: "010-7776-6665",
-      studentName: "이재일",
-    },
-    {
-      studentId: "2201393",
-      classId: "2-wdj",
-      studentContact: "010-8832-1234",
-      studentName: "이현민",
-    },
-  ];
   return (
     <>
       <Aside />
       <Display>
-        <Table type="student" data={data} />
+        {/* <Table type="student" data={data} /> */}
+        {/* <TableDemo /> */}
+        <PagingDemo />
       </Display>
     </>
   );
