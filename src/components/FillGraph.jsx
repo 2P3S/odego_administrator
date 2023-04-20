ce48f855c4eba6413d6292fdbcd6b6c37dc493fd
d556f8 <l.juhyeonni@gmail.com>
2023-04-20 11:38:53 +0900

    style: Add margin to improve style

--- src/components/FillGraph.jsx
+++ src/components/FillGraph.jsx
@@ -1,6 +1,6 @@
 const FillGraph = ({ color, data }) => {
   return (
-    <div className="flex h-32 w-32 p-1 justify-center items-center rounded-full bg-slate-800">
+    <div className="flex h-32 w-32 p-1 m-2 justify-center items-center rounded-full bg-slate-800">
       <div
         className={`flex rounded-full ${color} justify-center items-center text-zinc-100 text-xl font-bold`}
         style={{
