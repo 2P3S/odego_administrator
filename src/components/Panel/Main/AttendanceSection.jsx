a9f9d7166bdb6e7295c02685b9e1facc864c2f4d
d556f8 <l.juhyeonni@gmail.com>
2023-05-24 14:17:10 +0900

    refactor(AttendanceSection): Improve attendance section UI and structure

--- src/components/Panel/Main/AttendanceSection.jsx
+++ src/components/Panel/Main/AttendanceSection.jsx
@@ -3,21 +3,23 @@ import FillGraph from "@Components/common/FillGraph";
 
 const AttendanceSection = () => {
   return (
-    <div className="attendance-section">
+    <div className="attendance-section flex-col">
       <div className="header-sm my-3">
         <TitleBadge color="red">참석율</TitleBadge>
       </div>
-      <div>
-        참석 인원
-        <div>
-          {"29" + " / " + "33"}
+      <div className="attendance-ratio flex">
+        <div className="flex">
+          <div className="attend-ratio flex flex-col justify-center">
+            <div>참석 인원</div>
+            <div>{"29" + " / " + "33"}</div>
+          </div>
           <FillGraph color="bg-indigo-500" data={97} />
         </div>
-      </div>
-      <div>
-        미참석 인원
-        <div>
-          {"4" + " / " + "33"}
+        <div className="flex">
+          <div className="noshow-ratio flex flex-col justify-center">
+            <div>미참석 인원</div>
+            <div>{"4" + " / " + "33"}</div>
+          </div>
           <FillGraph color="bg-amber-400" data={3} />
         </div>
       </div>
