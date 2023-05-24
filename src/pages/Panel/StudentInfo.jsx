66cf7ee1c4771c2e1824d5375d8da21d29a9b66a
d556f8 <l.juhyeonni@gmail.com>
2023-05-24 14:14:57 +0900

    feat(panel): add student info section to display component

--- src/pages/Panel/StudentInfo.jsx
+++ src/pages/Panel/StudentInfo.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Aside from "@Components/Panel/Aside";
+import Display from "@Components/Panel/Display";
+import StudentInfoSection from "@Components/Panel/StudentInfo/StudentInfoSection";
 
 const StudentInfoPage = () => {
   return (
     <>
       <Aside />
+      <Display>
+        <StudentInfoSection />
+      </Display>
     </>
   );
 };
